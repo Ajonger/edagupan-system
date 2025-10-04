ff54882db37dc84f2eeae51e26d04deb85d1473b
Ajonger <Deguzmanaj729@gmail.com>
2025-10-04 21:07:00 +0800

    Commit to master

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+// Project Name
+define("SITE_NAME", "EDAGUPAN System");
+
+// Base URL (optional, helpful if you’ll link CSS/JS later)
+define("BASE_URL", "http://localhost/edagupan-system/");
+
+// Include DB Config
+require_once "dbconfig.php";
+?>
